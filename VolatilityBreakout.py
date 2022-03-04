3e28a3b46c518259caefcea846de1d6f9273444f
ServontiusT <zEGyRY28GH0iNbMiOqZ0UoJSmkU47ORkmxem1>
2022-03-03 20:21:21 -0600

    Created PY file of function to allow for importing

--- VolatilityBreakout.py
+++ VolatilityBreakout.py
@@ -1,24 +1,24 @@
 import numpy as np
-import pandas
+import pandas as pd
 
 
-def vol_breakout(dataframe: pandas.DataFrame, percentage: int):
-    # Convert int to Percentage
-    multiplier = percentage / 100
+def vol_breakout(dataframe, percentage: int):
+    # Convert csv file into Dataframe
+    dataframe = pd.read_csv(dataframe)
 
     # Calculate the Top and Bottom of the range to determine where positions will be taken
-    top_range_calc = dataframe['high'][-1] * multiplier
-    bottom_range_calc = dataframe['low'][-1] * multiplier
-    top_of_range = dataframe['open'] + top_range_calc
-    bottom_of_range = dataframe['open'] - bottom_range_calc
+    top_range_calc = (dataframe.iloc[-1]['High'] * percentage) / 100
+    bottom_range_calc = (dataframe.iloc[-1]['Low'] * percentage) / 100
+    top_of_range = dataframe['Open'] + top_range_calc
+    bottom_of_range = dataframe['Open'] - bottom_range_calc
 
     # Create Columns that hold the number of times a cross happens above the range, and generates a Signal
     dataframe['Top of Range'] = round(top_of_range, 2)
-    dataframe['Cross Above Range'] = np.where(dataframe['close'] > round(top_of_range, 2), 1, 0)
+    dataframe['Cross Above Range'] = np.where(dataframe['Close'] > round(top_of_range, 2), 1, 0)
     dataframe['Long Signal'] = dataframe['Cross Above Range'].diff()
 
     dataframe['Bottom of Range'] = round(bottom_of_range, 2)
-    dataframe['Cross Below Range'] = np.where(dataframe['close'] < round(bottom_of_range, 2), 2, 0)
+    dataframe['Cross Below Range'] = np.where(dataframe['Close'] < round(bottom_of_range, 2), 2, 0)
     dataframe['Short Signal'] = dataframe['Cross Below Range'].diff()
 
     signal_dict = {
